5a36d96fe979a238ef798347f2249b2c8f159160
youngmin-OS <z4567890@naver.com>
2025-11-26 14:37:14 +0900

    사분면 찾기(14681번)

---
+++ src/Bronze/QuadrantN.java
@@ -0,0 +1,31 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class QuadrantN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if(x > 0 && y > 0){
+            System.out.println("1");
+        }
+
+        else if(x < 0 && y > 0){
+            System.out.println("2");
+        }
+
+        else if(x < 0 && y < 0){
+            System.out.println("3");
+        }
+
+        else if(x > 0 && y < 0){
+            System.out.println("4");
+        }
+
+        else{
+            System.out.println("(0, 0)");
+        }
+    }
+}
