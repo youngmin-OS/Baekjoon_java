33f8494aff9acb4a9888882fb02f0bcde8211751
youngmin-OS <z4567890@naver.com>
2025-11-26 18:16:18 +0900

    단어 길이 재기(2743번)

---
+++ src/Bronze/StringLength.java
@@ -0,0 +1,12 @@
+package Bronze;
+import java.util.Scanner;
+
+public class StringLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s.length());
+
+
+    }
+}
