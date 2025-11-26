811b6af9b8c778a7bb20ab325cb0281b78dc4211
youngmin-OS <z4567890@naver.com>
2025-11-26 14:19:29 +0900

    태국 불기년도 -> 서기년도 (18108번)

---
+++ src/Bronze/ThailandAge.java
@@ -0,0 +1,11 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class ThailandAge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.println(year - 543);
+    }
+}
