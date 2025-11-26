7e05158eb0a7456f321ab4f1bea88ff7a6d29f4e
youngmin-OS <z4567890@naver.com>
2025-11-26 14:15:08 +0900

    ??!(10926번)

---
+++ src/Bronze/CheckSame.java
@@ -0,0 +1,10 @@
+package Bronze;
+import java.util.Scanner;
+
+public class CheckSame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        System.out.println(id + "??!");
+    }
+}
